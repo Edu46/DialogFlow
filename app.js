4a08bb35e4cf12bd732a24e66f32c5b4f5d2552a
Eduardo AG <eduardo.alvarez@emergys.com>
2021-04-22 09:20:02 -0500

    Update welocme flow

--- app.js
+++ app.js
@@ -42,184 +42,236 @@ app.post('/', express.json(), (req, res) => {
     console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
     console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
 
-    async function welcome(agent) {
-        agent.add(`Hola  👋 !!, Bienvenido a Edd´s Pizza. 🍕 `);
+    function welcome(agent) {
+        agent.add(`Hola  👋 !!, Bienvenido a Edd´s Pizza. 🍕`);
+        agent.add(`Cual es tu nombre?`);
+        agent.context.set({
+            'name':'name-user',
+            'lifespan': 3,
+        })
+    }
+
+    async function obtainName(agent){
+        const name = agent.context.get('name-user');
+        agent.add(`Saludos ${agent.parameters.name.name}`);
+        agent.add(`Es correcto tu nombre?`);
+
+        agent.context.set({name:'awaiting-name-user', lifespan: 2,});
+        agent.add(new Suggestion(`Sí`));
+        agent.add(new Suggestion(`No`));
+
+    }
+
+    async function obtainNameAll(agent){
+        const name = agent.context.get('name-user');
+        agent.add(`Saludos ${agent.parameters.name}`);
+        agent.add(`Es correcto tu nombre?`);
+
+        agent.context.set({name:'awaiting-name-user', lifespan: 2,})
+
+        agent.add(new Suggestion(`Sí`));
+        agent.add(new Suggestion(`No`));
+
+    }
+
+    
+    function obtainNameNo(agent){
+        const name = agent.context.get('name-user');
+        agent.add(`No hay problema, puedes repetir tu nombre por favor?`);
+        agent.context.set({name:'name-user', lifespan: 3,
+            parameters: {
+                nameParameter: agent.parameters.name,
+            }
+        })
+    }
+
+    async function obtainNameYes(agent){
+        agent.context.set({name:'awaiting-name-user', lifespan: 0,})
+        agent.add(`Que tamaño de Pizza desea ordenar? 🙇`);
         const tamaniosPizza = await servicio.obtenerTamanios();
         for(let tamanioPizza of tamaniosPizza){
             agent.add(new Suggestion(tamanioPizza));
         } 
-        agent.context.set({
-            'name':'tamano-pizza',
-            'lifespan': 10,
-            'parameters':{
-              'tamano':agent.parameters.tamano,
-              }
-        });
-        agent.add(`Cual es tu nombre?`);
-    }
 
-    function obtainName(agent){
-        agent.context.set({
-            'name':'obtain-name',
-            'lifespan': 10,
-            'parameters':{
-              'tamano':agent.parameters.name,
-            }
-        })
-        agent.add(`que tamaño de Pizza desea ordenar? 🙇`);
     }
 
     function sizePizza(agent) {
+        const context = agent.context.get('name-user');
+        agent.context.set({
+            'name':'size-pizza',
+            'lifespan': 2,
+            'parameters':{
+                'tamano':agent.parameters.tamano,
+                'name': context.parameters.name
+            }
+        });
+        agent.context.set({name: 'name-user', lifespan:0});
         agent.add(`Genial ! 👏`);
-        agent.add(`Todas nuestras  Pizzas cuentan con el mejor queso 🧀 y salsa 🍅 Italiana 👌, deseas ver la lista de ingredientes? 🙇`);
+        agent.add(`Todas nuestras  Pizzas cuentan con el mejor queso 🧀 y salsa 🍅 Italiana 👌, 
+        deseas ver la lista de ingredientes? 🙇`);
         
         agent.add(new Suggestion('Sí'));
         agent.add(new Suggestion('No'));
+        
     }
 
     async function ingredientsYes(agent){
         //Añadir validacion de ingrediente no disponible
+        const context = agent.context.get('size-pizza');
         const ingredientes = await servicio.ingredientes();
+        console.log(context);
         agent.add(`Estos son los disponibles: ${ingredientes}`);
         agent.context.set({
-            'name':'ingredientes-pizza',
-            'lifespan': 10,
+            'name':'ingredients-pizza',
+            'lifespan': 2,
             'parameters':{
-              'tamano':agent.parameters.ingrediente,
+              'ingredientes':agent.parameters.ingrediente,
+              'tamano':context.parameters.tamano,
+              'name': context.parameters.name
+              
             }
         });
-
     }
 
     function ingredientsNo(agent){
+        const context = agent.context.get('size-pizza');
         //Añadir validacion de ingrediente no disponible
         agent.add(`Que ingrediente deseas?`);
+
         agent.context.set({
-            'name':'ingredientes-pizza',
-            'lifespan': 10,
+            'name':'ingredients-pizza',
+            'lifespan': 2,
             'parameters':{
-              'tamano':agent.parameters.ingrediente,
+              'ingredientes':agent.parameters.ingrediente,
+              'tamano':context.parameters.tamano,
+              'name': context.parameters.name
+              
             }
         });
     }
 
     function ingredientsPizza(agent){
         agent.add(`Excelente decisión:`);
-        const response = agent.context.get('ingredientes-pizza');
-        agent.add(`${response.parameters.ingredinete}`);
+        const context = agent.context.get('ingredients-pizza');
+        agent.add(`${context.parameters.any}`);
+        console.log(context);
         agent.add(`Pizza lista!!, me podrias ayudar proporcionando los siguientes datos por favor? 😀`);
         agent.add(`Dirección :`);
     
     }
 
-    function obtainedAddress(agent){
-        agent.context.set({
-            'name':'domicilio-obtenido',
-            'lifespan': 5,
-            'parameters':{
-              'location':agent.parameters['street-address'],
-              }
-        });
-        agent.add(`Telefono :`);
-    }
+    // function obtainedAddress(agent){
+    //     agent.context.set({
+    //         'name':'domicilio-obtenido',
+    //         'lifespan': 5,
+    //         'parameters':{
+    //           'location':agent.parameters['street-address'],
+    //           }
+    //     });
+    //     agent.add(`Telefono :`);
+    // }
 
-    function obtainedNumber(agent){
-        agent.context.set({
-            'name': 'obtener-numero',
-            'lifespan':5,
-            'parameters':{
-                'numero': agent.parameters['phone-number'],
-            }
-        });
+    // function obtainedNumber(agent){
+    //     agent.context.set({
+    //         'name': 'obtener-numero',
+    //         'lifespan':5,
+    //         'parameters':{
+    //             'numero': agent.parameters['phone-number'],
+    //         }
+    //     });
         
-        agent.add(`Listo!! tu Pizza llegara pronto... 🙌`);
+    //     agent.add(`Listo!! tu Pizza llegara pronto... 🙌`);
 
-        agent.add(`Por ultimo me gustaria confirmar tu pedido...  👨‍🍳`);
+    //     agent.add(`Por ultimo me gustaria confirmar tu pedido...  👨‍🍳`);
 
-        const tamano = agent.context.get('tamano-pizza');
-        const ingrediente = agent.context.get('ingredientes-pizza');
-        const  direccion = agent.context.get('domicilio-obtenido');
+    //     const tamano = agent.context.get('tamano-pizza');
+    //     const ingrediente = agent.context.get('ingredientes-pizza');
+    //     const  direccion = agent.context.get('domicilio-obtenido');
 
-        agent.add(`Tamaño 🍕 : ${tamano.parameters.tamano}`);
-        agent.add(`Ingredientes 🧾 : ${ingrediente.parameters.ingredinete}`);
-        agent.add(`Dirección 🏡 : ${direccion.parameters.location['street-address'] + ' ' + direccion.parameters.location['subadmin-area']}`);
-        agent.add(`Numero de contacto 📱 : ${agent.parameters['phone-number']}`)
+    //     agent.add(`Tamaño 🍕 : ${tamano.parameters.tamano}`);
+    //     agent.add(`Ingredientes 🧾 : ${ingrediente.parameters.ingredinete}`);
+    //     agent.add(`Dirección 🏡 : ${direccion.parameters.location['street-address'] + ' ' + direccion.parameters.location['subadmin-area']}`);
+    //     agent.add(`Numero de contacto 📱 : ${agent.parameters['phone-number']}`)
 
-        updateIngredients();
+    //     updateIngredients();
 
-        agent.add(new Suggestion('Sí'));
-        agent.add(new Suggestion('No'));
-    }
+    //     agent.add(new Suggestion('Sí'));
+    //     agent.add(new Suggestion('No'));
+    // }
 
-    //Lomas de las palmas 456 sentido comumo int 6 guadalajara jalisco
+    // //Lomas de las palmas 456 sentido comumo int 6 guadalajara jalisco
 
-    async function addressYes(agent){
-        agent.add(`Listo, llegara en unos minutos tu Pizza`)
+    // async function addressYes(agent){
+    //     agent.add(`Listo, llegara en unos minutos tu Pizza`)
 
-        const tamano = agent.context.get('tamano-pizza');
-        const ingrediente = agent.context.get('ingredientes-pizza');
-        const numero = agent.context.get('obtener-numero');
-        const direccion = agent.context.get('domicilio-obtenido');
+    //     const tamano = agent.context.get('tamano-pizza');
+    //     const ingrediente = agent.context.get('ingredientes-pizza');
+    //     const numero = agent.context.get('obtener-numero');
+    //     const direccion = agent.context.get('domicilio-obtenido');
 
-        const pedido = {
-            tamano:tamano.parameters.tamano,
-            ingrediente:ingrediente.parameters.ingredinete,
-            direccion: direccion.parameters.location['street-address'] + ' ' + direccion.parameters.location['subadmin-area'],
-            numero:numero.parameters.numero,
-        }
+    //     const pedido = {
+    //         tamano:tamano.parameters.tamano,
+    //         ingrediente:ingrediente.parameters.ingredinete,
+    //         direccion: direccion.parameters.location['street-address'] + ' ' + direccion.parameters.location['subadmin-area'],
+    //         numero:numero.parameters.numero,
+    //     }
 
-        const pedidoBD = await servicio.guardarPedidosDialog(pedido);
-        console.log(pedidoBD + 'return de bd');
-        agent.add(`Tu id de orden es la: ${pedidoBD._id}`);
+    //     const pedidoBD = await servicio.guardarPedidosDialog(pedido);
+    //     console.log(pedidoBD + 'return de bd');
+    //     agent.add(`Tu id de orden es la: ${pedidoBD._id}`);
 
 
 
-    }
+    // }
 
-    function addressNo(agent){
-        agent.add(`Para modificar tu pedido puedes repetir los ingredientes, la dirección o el tamaño por el que deseas cambiar`);
-    }
+    // function addressNo(agent){
+    //     agent.add(`Para modificar tu pedido puedes repetir los ingredientes, la dirección o el tamaño por el que deseas cambiar`);
+    // }
 
     function fallback(agent) {
         agent.add(`Lo siento, puedes intentar de nuevo?`);
     }
 
-    async function updateIngredients(){
-        const ingredientesBD = await servicio.ingredientesBD();
-        const pedidoBD = await servicio.obtenerPedidos();
+    // async function updateIngredients(){
+    //     const ingredientesBD = await servicio.ingredientesBD();
+    //     const pedidoBD = await servicio.obtenerPedidos();
 
-        let cantidad = 0;
+    //     let cantidad = 0;
 
-        console.log(pedidoBD);
-        console.log(ingredientesBD);
+    //     console.log(pedidoBD);
+    //     console.log(ingredientesBD);
 
-        for(let i=0; i<= pedidoBD.ingrediente.length-1; i++){
-            let  ingrediente = pedidoBD.ingrediente[i];
-            for(let i=0; i<= ingredientesBD.length-1; i++){
-                if(ingrediente == ingredientesBD[i].ingrediente){
-                    cantidad = ingredientesBD[i].cantidad - 1;
-                    const ingredienteUpdate = {
-                        id: ingredientesBD[i]._id,
-                        ingrediente: ingredientesBD[i].ingrediente,   
-                        cantidad: cantidad 
-                    }
-                    console.log(ingredienteUpdate);
-                }
-            }
-        }
-    }
+    //     for(let i=0; i<= pedidoBD.ingrediente.length-1; i++){
+    //         let  ingrediente = pedidoBD.ingrediente[i];
+    //         for(let i=0; i<= ingredientesBD.length-1; i++){
+    //             if(ingrediente == ingredientesBD[i].ingrediente){
+    //                 cantidad = ingredientesBD[i].cantidad - 1;
+    //                 const ingredienteUpdate = {
+    //                     id: ingredientesBD[i]._id,
+    //                     ingrediente: ingredientesBD[i].ingrediente,   
+    //                     cantidad: cantidad 
+    //                 }
+    //                 console.log(ingredienteUpdate);
+    //             }
+    //         }
+    //     }
+    // }
 
 
     let intentMap = new Map();
     intentMap.set('welcome.client', welcome);
+    intentMap.set('name.user.obtained', obtainName);
+    intentMap.set('name.user.obtained.catch.all', obtainNameAll);
+    intentMap.set('name.user.obtained - yes', obtainNameYes);
+    intentMap.set('name.user.obtained - no', obtainNameNo);
     intentMap.set('pizza.size.obtained', sizePizza);
-    intentMap.set('ingredients-yes',ingredientsYes);
+    intentMap.set('ingredients - yes',ingredientsYes);
     intentMap.set('ingredients', ingredientsPizza);
-    intentMap.set('ingredients-no',ingredientsNo);
-    intentMap.set('address.obtained', obtainedAddress);
-    intentMap.set('number.obtained',obtainedNumber),
-    intentMap.set('address-yes',addressYes);
-    intentMap.set('address-no',addressNo);
+    intentMap.set('ingredients - no',ingredientsNo);
+    // intentMap.set('address.obtained', obtainedAddress);
+    // intentMap.set('number.obtained',obtainedNumber),
+    // intentMap.set('address-yes',addressYes);
+    // intentMap.set('address-no',addressNo);
     intentMap.set('fallback.client', fallback);
     agent.handleRequest(intentMap);
 
